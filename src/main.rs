c9de87228bfab2f1c975218ad897da1a2364939f
Maniram Yadav <26573822+maniram-yadav@users.noreply.github.com>
2025-01-22 17:59:28 +0530

    code added in the modules

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 
+mod array;
+
 fn main(){
+    let  arr = vec!{91,12,3,78};
+
+    let mut clone = arr.clone();
+    array::bubble::sort(&mut clone);
+    println!("Sorted array {:?}",clone);
     
+    
+    clone = arr.clone();
+    array::selection::sort(&mut clone);
+    println!("Sorted array {:?}",clone);
 }
