af32c790db9a147dd3b95ac36a269e966c60a6ff
maniram <maniramyadav786@gmail.com>
2025-09-08 09:29:48 +0000

    dining philospher implemntation

--- src/concurrent/bank.rs
+++ src/concurrent/bank.rs
@@ -1,14 +1,14 @@
 use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
-
+use std::sync::Arc;
 
 struct BankAccount {
-    balance: Mutex<f64> ,
+    balance: Mutex<f64>,
     account_numer: String,
 }
 
-impl BankAccount    {
+impl BankAccount {
     pub fn new(account_number: String, initial_balance: f64) -> Self {
         BankAccount {
             balance: Mutex::new(initial_balance),
@@ -16,45 +16,53 @@ impl BankAccount    {
         }
     }
 
-    pub fn deposit(&mut self,amount:f64) -> Self{
-
+    pub fn deposit(&mut self, amount: f64) -> Self {
         let mut bal = self.balance.lock().unwrap();
         *bal += amount;
-        println!("Deposited {} to account {}. New balance: {}", amount, self.account_numer, *bal);
+        println!(
+            "Deposited {} to account {}. New balance: {}",
+            amount, self.account_numer, *bal
+        );
         thread::sleep(Duration::from_millis(50));
-        Self::new(self.account_numer.clone(),*bal)
-
+        Self::new(self.account_numer.clone(), *bal)
     }
 
-    pub fn transfer(&mut self,amount:f64,target:&mut BankAccount) ->  Result<(), String> {
-       
-        let (mut source_lock, mut target_lock ) = if self as *const _ < target as *const _ {
-                   let s =  self.balance.lock().map_err(|e| e.to_string())?;
-                   let t =  target.balance.lock().map_err(|e| e.to_string())?;
-                   (s,t)
-                } else {
-                    let t = target.balance.lock().map_err(|e| e.to_string())?;
-                    let s = self.balance.lock().map_err(|e| e.to_string())?;
-                    (s,t)
-                };
-            if *source_lock < amount {
-                println!("Insufficient funds in account {}. Transfer of {} failed.", self.account_numer, amount);
-                return Err("Insufficient funds".to_string());
-            }  
-            *source_lock -= amount;
-            *target_lock += amount;
-            println!("Transferred {} from account {} to account {}. New balances: {} -> {}, {} -> {}", amount, self.account_numer, target.account_numer, self.account_numer, *source_lock, target.account_numer, *target_lock);
-            thread::sleep(Duration::from_millis(50));
-            Ok(())
-
+    pub fn transfer(&mut self, amount: f64, target: &mut BankAccount) -> Result<(), String> {
+        let (mut source_lock, mut target_lock) = if self as *const _ < target as *const _ {
+            let s = self.balance.lock().map_err(|e| e.to_string())?;
+            let t = target.balance.lock().map_err(|e| e.to_string())?;
+            (s, t)
+        } else {
+            let t = target.balance.lock().map_err(|e| e.to_string())?;
+            let s = self.balance.lock().map_err(|e| e.to_string())?;
+            (s, t)
+        };
+        if *source_lock < amount {
+            println!(
+                "Insufficient funds in account {}. Transfer of {} failed.",
+                self.account_numer, amount
+            );
+            return Err("Insufficient funds".to_string());
         }
-    pub fn get_balance(&self) -> f64{
+        *source_lock -= amount;
+        *target_lock += amount;
+        println!(
+            "Transferred {} from account {} to account {}. New balances: {} -> {}, {} -> {}",
+            amount,
+            self.account_numer,
+            target.account_numer,
+            self.account_numer,
+            *source_lock,
+            target.account_numer,
+            *target_lock
+        );
+        thread::sleep(Duration::from_millis(50));
+        Ok(())
+    }
+    pub fn get_balance(&self) -> f64 {
         let bal = self.balance.lock().unwrap();
         *bal
     }
-
-    
-
 }
 
 mod tests {
@@ -76,4 +84,38 @@ mod tests {
         assert_eq!(account1.get_balance(), 900.0);
         assert_eq!(account2.get_balance(), 800.0);
     }
+
+    //   #[test]
+    // fn concurrent_transfers() {
+        
+    //     let account_a = Arc::new(BankAccount::new("A8989".to_string(),1000.0));
+    //     let account_b = Arc::new(BankAccount::new("45B6".to_string(),500.0));
+    //     let handles: Vec<_> = (0..10)
+    //         .map(|i| {
+    //             let  a = Arc::clone(&account_a);
+    //             let b = Arc::clone(&account_b);
+
+    //             thread::spawn(move || {
+    //                 thread::sleep(Duration::from_millis(i * 10));
+    //                 if i % 2 == 0 {
+    //                     a.transfer( 50.0,&mut Arc::clone(&account_b)).unwrap()
+                    
+    //                 } else {
+    //                     b.transfer(25.0,&mut a).unwrap()
+    //                 }
+    //             })
+    //         })
+    //         .collect();
+    //     for handle in handles {
+    //         handle.join().unwrap();
+    //         println!(
+    //             "Final balances - A: {:.2}, B: {:.2}",
+    //             *account_a.balance.lock().unwrap(),
+    //             *account_b.balance.lock().unwrap()
+    //         );
+    //     }
+
+    // }
+
+    
 }
---
+++ src/concurrent/dining_philospher.rs
@@ -0,0 +1,71 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+struct Philospher {
+    name: String,
+    left_fork: Arc<Mutex<i32>>,
+    right_fork: Arc<Mutex<i32>>,
+}
+
+impl Philospher {
+    pub fn eat(&self) {
+        let left = self.left_fork.try_lock();
+        if left.is_err() {
+            println!("{} failed to acquire left fork", self.name);
+            thread::sleep(Duration::from_millis(100));
+            return;
+        }
+
+        thread::sleep(Duration::from_millis(100));
+
+        match self.right_fork.try_lock() {
+            Ok(_) => {
+                println!("{} is eating!", self.name);
+                thread::sleep(Duration::from_millis(100));
+                println!("{} has finished eating", self.name);
+            }
+            Err(_) => {
+                println!("{} failed to acquire right fork", self.name);
+                thread::sleep(Duration::from_millis(100));
+            }
+        }
+    }
+
+    pub fn think(&self) {
+        println!("{} is thinking", self.name);
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    pub fn dining_cycle(&self) {
+        let chopsticks = (0..5).map(|_| Arc::new(Mutex::new(0))).collect::<Vec<_>>();
+        let philosphers: Vec<_> = (0..5)
+            .map(|i| {
+                let left = chopsticks[i].clone();
+                let right = chopsticks[(i + 1) % 5].clone();
+
+                Philospher {
+                    name: format!("Philospher {}", i + 1),
+                    left_fork: left,
+                    right_fork: right,
+                }
+            })
+            .collect();
+
+        let handles: Vec<_> = philosphers
+            .into_iter()
+            .map(|p| {
+                thread::spawn(move || {
+                    for _ in 0..5 {
+                        p.eat();
+                        p.think();
+                    }
+                })
+            })
+            .collect::<Vec<_>>();
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+    }
+}
