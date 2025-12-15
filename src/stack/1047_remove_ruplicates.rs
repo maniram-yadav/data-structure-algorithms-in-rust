d0f4f17a35b059b464e9ef180c166b98f642d65f
maniram <maniramyadav786@gmail.com>
2025-12-15 09:34:01 +0000

    remove duplcicates

---
+++ src/stack/1047_remove_ruplicates.rs
@@ -0,0 +1,22 @@
+// 1047. Remove All Adjacent Duplicates In String
+
+
+impl Solution {
+    pub fn remove_duplicates(s: String) -> String {
+       let mut stack:Vec<char> = Vec::new();
+       for ch in s.chars() {
+
+            if let Some(&top) = stack.last() {
+                    if top==ch {
+                        stack.pop();
+                    } else {
+                    stack.push(ch);
+                    }
+
+            } else {
+                    stack.push(ch);
+            }
+       }
+       stack.into_iter().collect()
+    }
+}
